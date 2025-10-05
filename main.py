2e0d4adc9b817a67fb096213448b26911293af6f
RafaelaHH <hhj0034735@est.univalle.edu>
2025-10-04 22:57:43 -0400

    correccion errores.16 en firestore_to_openseahc.py

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from typing import List
 from buscar_recetas import buscar_recetas, limpiar_stopwords
+from fastapi import FastAPI
+from opensearchpy import OpenSearch
+import os
 
 app = FastAPI(title="Buscador de Recetas")
+
+host = os.getenv('OPENSEARCH_HOST', 'localhost')
+port = int(os.getenv('OPENSEARCH_PORT', 443))
+user = os.getenv('OPENSEARCH_USERNAME', 'admin')
+password = os.getenv('OPENSEARCH_PASSWORD', 'admin')
+
+client = OpenSearch(
+    hosts=[{"host": host, "port": port}],
+    http_auth=(user, password),
+    use_ssl=True,
+    verify_certs=True,
+)
 
 @app.get("/")
 def read_root():
@@ -26,3 +41,13 @@ def buscar(query: str = Query(..., description="Palabras clave para buscar recet
         return {"resultados": resultados}
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
+
+@app.get("/ping")
+def ping_opensearch():
+    try:
+        if client.ping():
+            return {"status": "OpenSearch OK"}
+        else:
+            return {"status": "OpenSearch no responde"}
+    except Exception as e:
+        return {"error": str(e)}
